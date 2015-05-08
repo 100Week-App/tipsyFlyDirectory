0bbcb37490f34f8fb174131f0ea937616d36f96e
mitesh-100week <miteshmaheta@Rishads-MacBook-Pro.local>
2015-05-08 15:54:32 +0530

    First time commit

---
+++ tipsyFlyDirectory/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  tipsyFlyDirectory
+//
+//  Created by Mitesh Maheta on 08/05/15.
+//  Copyright (c) 2015 tipsy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ tipsyFlyDirectory/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  tipsyFlyDirectory
+//
+//  Created by Mitesh Maheta on 08/05/15.
+//  Copyright (c) 2015 tipsy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
